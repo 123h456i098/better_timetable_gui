f841a4422f07fd3c6dfba4ab700923a6211b9472
Glompson <78880775+123h456i098@users.noreply.github.com>
2022-07-15 20:32:04 +1200

    Basic model structure started
    Classes to do with teacher, subject, and timings are set up, ready to be used in timetable_model.py, possible changes to make: set the object typings to be the actual class objects that they will be.

---
+++ classes_model.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class teacher:
+    _firstname: str
+    _lastname: str
+
+    @property
+    def name(self) -> str:
+        return f"{self._firstname} {self._lastname}".lower().title()
+
+    @property
+    def code(self) -> str:
+        return f"{self._lastname[0]}{self._firstname[:1]}".upper()
+
+
+@dataclass
+class subject:
+    _code: str
+    _subject: str
+    _teacher: object
+
+    @property
+    def code(self) -> str:
+        return self._code
+
+    @property
+    def subject(self) -> str:
+        return self._subject
+
+    @property
+    def teacher(self) -> object:
+        return self._teacher
+
+
+@dataclass
+class timings:
+    _start_hour: int
+    _start_min: int
+    _end_hour: int
+    _end_min: int
+
+    @property
+    def start(self) -> str:
+        return f"{self._start_hour}:{self._start_min}"
+
+    @property
+    def end(self) -> str:
+        return f"{self._end_hour}:{self.end_min}"
+
+
+@dataclass
+class subject_instance:
+    _subject: object
+    _room: str
+    _times: object
+
+    @property
+    def subject(self) -> object:
+        return self._subject
+
+    @property
+    def room(self) -> str:
+        return self._room
+
+    @property
+    def times(self) -> object:
+        return self._times
